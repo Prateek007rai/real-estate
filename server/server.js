56a508cd4cc0fd6c7759aeefb3f1c772477c4182
Prateek <prateek.rai@moondive.co>
2023-07-12 15:41:10 +0530

    final-----today

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const morgan = require("morgan");
 const cors = require("cors");
 const db = require('./con_fig/mongoose');
